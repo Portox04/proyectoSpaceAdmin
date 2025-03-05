cb69c5c7c7fb161e30df77378adbd2a96929b432
Erick Portocarrero <dibtecportocarrero@gmail.com>
2025-03-05 11:56:39 -0600

    Pruebas con java Swing con Botones y Paneles 3x2

--- proyectoSpaceAdmin/src/proyectospaceadmin/ProyectoSpaceAdmin.java
+++ proyectoSpaceAdmin/src/proyectospaceadmin/ProyectoSpaceAdmin.java
@@ -5,16 +5,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 //Rama ERICK P
-
 package proyectospaceadmin;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -27,30 +28,61 @@ public class ProyectoSpaceAdmin {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        //Ventana Prueba
-        JFrame frame = new JFrame("Mi Primera ventana"); 
-        //Seleccionar el tamano de la ventana
-        frame.setSize(400,300);
-        //Indica que se cierre la ventana con la X
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Indica que se vea la pantalla
-        frame.setVisible(true);
-        
-        JLabel label = new JLabel("Preciona aqui:");
-        JButton button = new JButton("Click aqui");
-        
-        //Evento del boton 
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                label.setText("¡Botón presionado!");
-            }
-        });
-        
-        frame.add(label);
-        frame.add(button);
-        frame.setVisible(true);
+//        
+//        ***BOTON CON TEXTO DE CONFIRMACION DENTROD DE LA PANTALLA***
+//        //Ventana Prueba
+//        JFrame frame = new JFrame("Boton"); 
+//        //Seleccionar el tamano de la ventana
+//        frame.setSize(400,300);
+//        //Indica que se cierre la ventana con la X
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        //Organizar todos los elemento de izquierda a derecha
+//        frame.setLayout(new FlowLayout());
+//        
+//        JLabel label = new JLabel("Preciona aqui:");
+//        JButton button = new JButton("Click aqui");
+//        
+//        //Lo que llegara a hacer el boton
+//        button.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                label.setText("¡Botón presionado!");
+//            }
+//        });
+//        
+//        frame.add(label);
+//        frame.add(button);
+//        //Indica que se vea la pantalla
+//        frame.setVisible(true);
 
+//      ***PANEL Y CAMPOS DE TEXTO***
+        
+//      Creacion de ventana y ajustes
+        JFrame frame = new JFrame("Formulario de Usuario");
+        frame.setSize(400,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Hace una cuadricula el GridLayout en este caso de 3x2
+        frame.setLayout(new GridLayout(3,2));
+        
+        JLabel lblName = new JLabel("Nombre");
+        JTextField txtName = new JTextField(20);   //Es un input de texto
+        JButton btnSend = new JButton("Enviar");
+        JLabel lblMessage = new JLabel("");
+        
+        //ACCION QUE HARA EL BOTON (btnSend es el boton que le accinaremos dicha accion)
+        btnSend.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String name = txtName.getText();
+                lblMessage.setText("Hola "+name);
+            }});
+                
+        frame.add(lblName);
+        frame.add(txtName);
+        frame.add(btnSend);
+        frame.add(lblMessage);
+        
+        frame.setVisible(true);
+            
+            
     }
-    
+
 }
